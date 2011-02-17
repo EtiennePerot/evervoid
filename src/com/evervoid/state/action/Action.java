6bd0e19bf69921b5c37c52383da93d953772e8b1
EtiennePerot <etienne@perot.me>
2011-02-17 01:44:45 +0000

    Base Action class

--- src/com/evervoid/state/action/Action.java
+++ src/com/evervoid/state/action/Action.java
@@ -2,27 +2,41 @@ package com.evervoid.state.action;
 
 import com.evervoid.json.Json;
 import com.evervoid.json.Jsonable;
+import com.evervoid.state.EverVoidGameState;
 import com.evervoid.state.player.Player;
 
 public abstract class Action implements Jsonable
 {
 	private final String aActionType;
-	private final String aPlayerName;
+	protected final Player aPlayer;
+	protected final EverVoidGameState aState;
 
-	public Action(final Player player, final String actionType)
+	public Action(final Json j, final EverVoidGameState state)
 	{
-		aPlayerName = player.getName();
+		aState = state;
+		aPlayer = aState.getPlayerByName(j.getStringAttribute("player"));
+		aActionType = j.getStringAttribute("actiontype");
+	}
+
+	public Action(final Player player, final String actionType, final EverVoidGameState state)
+	{
+		aState = state;
+		aPlayer = player;
 		aActionType = actionType;
 	}
+
+	protected abstract void execute();
 
 	public String getActionType()
 	{
 		return aActionType;
 	}
 
+	public abstract boolean isValid();
+
 	@Override
 	public Json toJson()
 	{
-		return new Json().setStringAttribute("player", aPlayerName).setStringAttribute("actiontype", getActionType());
+		return new Json().setStringAttribute("player", aPlayer.getName()).setStringAttribute("actiontype", getActionType());
 	}
 }
