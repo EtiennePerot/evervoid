94416319db994bbc5b43afd584a3fc6bd05b7498
vbonnet <valentinb@sbcglobal.net>
2011-02-20 04:48:59 +0000

    LobbyView listens for messages.

--- src/com/evervoid/client/views/lobby/LobbyView.java
+++ src/com/evervoid/client/views/lobby/LobbyView.java
@@ -1,17 +1,43 @@
 package com.evervoid.client.views.lobby;
 
+import com.evervoid.client.EVClientEngine;
 import com.evervoid.client.graphics.GraphicManager;
 import com.evervoid.client.graphics.Sprite;
+import com.evervoid.client.interfaces.EVLobbyMessageListener;
 import com.evervoid.client.ui.BoxControl;
 import com.evervoid.client.ui.Sizer.SizerDirection;
 import com.evervoid.client.views.Bounds;
+import com.evervoid.json.Json;
 import com.jme3.app.SimpleApplication;
 
-public class LobbyView extends SimpleApplication
+public class LobbyView extends SimpleApplication implements EVLobbyMessageListener
 {
 	public static void main(final String[] args)
 	{
 		new LobbyView().start();
+	}
+
+	public LobbyView()
+	{
+		EVClientEngine.registerLobbyListener(this);
+	}
+
+	@Override
+	public void receivedGameData(final Json gameData)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void receivedPlayerData(final Json playerData)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void receivedStartGame()
+	{
+		// TODO Auto-generated method stub
 	}
 
 	@Override
