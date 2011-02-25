3ae87eb7c7e172e54e5f10a4409eac7637f550d5
vbonnet <valentinb@sbcglobal.net>
2011-02-25 17:26:07 +0000

    added Exception for bad Action creation.

---
+++ src/com/evervoid/state/action/IllegalEVActionException.java
@@ -0,0 +1,14 @@
+package com.evervoid.state.action;
+
+public class IllegalEVActionException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IllegalEVActionException(final String message)
+	{
+		super(message);
+	}
+}
