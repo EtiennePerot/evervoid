93a7037554843fd42933b0d89a22eeed5882e3c4
vbonnet <valentinb@sbcglobal.net>
2011-03-24 16:16:21 +0000

    Adding a generic Pair Class.

---
+++ src/com/evervoid/utils/Pair.java
@@ -0,0 +1,38 @@
+package com.evervoid.utils;
+
+public class Pair<K, V>
+{
+	private final K aKey;
+	private V aValue;
+
+	public Pair(final K key, final V value)
+	{
+		aKey = key;
+		aValue = value;
+	}
+
+	public K getKe()
+	{
+		return aKey;
+	}
+
+	public V getValue()
+	{
+		return aValue;
+	}
+
+	public boolean setValue(final V value)
+	{
+		if (value == null) {
+			return false;
+		}
+		aValue = value;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "(" + aKey.toString() + ", " + aValue.toString() + ")";
+	}
+}
